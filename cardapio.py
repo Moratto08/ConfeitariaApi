bc4eab9474aefb9aa1bb684644adfe2ec08bcc97
ramon.moratto <ramon.moratto@gmail.com>
2025-11-28 22:59:23 +0000

    start project

---
+++ cardapio.py
@@ -0,0 +1,16 @@
+def get_cardapio():
+    cardapio = [
+        {
+            'id': 1,
+            'titulo': 'bolo de Chocolate',
+            'descricao': 'Delicioso bolo de chocolate',
+            'foto': ''
+        },
+        {
+            'id': 2,
+            'titulo': 'bolo de Morango',
+            'descricao': 'Delicioso bolo de morango',
+            'foto' : ''
+        }
+    ]
+    return cardapio
